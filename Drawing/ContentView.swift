31acb00c414aa4927422dbeb982f803fdad6513e
garima94921 <gaarimabothra@gmail.com>
2020-07-02 02:59:38 +0530

    Added: drawingGroup in colour cycling structure

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -26,12 +26,45 @@ struct Flower: Shape {
     }
 }
 
-struct ContentView: View {
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
 
-    @State private  var petalOffset = -20.0
-    @State private  var petalWidth = 100.0
     var body: some View {
-        VStack {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+    .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct ContentView: View {
+    //MARK: To DRAW FLOWER
+//    @State private  var petalOffset = -20.0
+//    @State private  var petalWidth = 100.0
+
+    //MARK: TO DRAW COLOUR CYCLE
+    @State private var colorCycle = 0.0
+
+    var body: some View {
+      /*  VStack {
             Flower(petalOffset: petalOffset, petalWidth: petalWidth)
                 .fill(Color.purple, style: FillStyle(eoFill: true))
             Text("Offset")
@@ -40,6 +73,14 @@ struct ContentView: View {
             Text("Width")
             Slider(value: $petalWidth, in: -40...40)
             .padding([.horizontal, .bottom])
+        }
+    }   */
+    VStack {
+            ColorCyclingCircle(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
+
+            Slider(value: $colorCycle)
+                .padding(.horizontal)
         }
     }
 }
